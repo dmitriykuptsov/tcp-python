3ea958a3ae5f266e96090b7f6506bb533be293d7
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2024-09-23 11:48:04 +0100

    Added TCP state variables

--- tcp.py
+++ tcp.py
@@ -15,3 +15,73 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from packets import TCPPacket, IPv4Packet
+
+class TransmissionControlBlock():
+    def __init__(self):
+        self.snd_una = 0
+        self.snd_nxt = 0
+        self.snd_wnd = 0
+        self.snd_up = 0
+        self.snd_wl1 = 0
+        self.snd_wl2 = 0
+        self.iss = 0
+        self.rcv_nxt = 0
+        self.rcv_wnd = 0
+        self.rcv_up = 0
+        self.irs = 0
+    def snd_una(self, value = None):
+        if value:
+            self.snd_una = value
+        else:
+            return self.snd_una
+    def snd_nxt(self, value = None):
+        if value:
+            self.snd_nxt = value
+        else:
+            return self.snd_nxt
+    def snd_wnd(self, value = None):
+        if value:
+            self.snd_wnd = value
+        else:
+            return self.snd_wnd
+    def snd_up(self, value = None):
+        if value:
+            self.snd_up = value
+        else:
+            return self.snd_up
+    def snd_wl1(self, value = None):
+        if value:
+            self.snd_wl1 = value
+        else:
+            return self.snd_wl1
+    def snd_wl2(self, value = None):
+        if value:
+            self.snd_wl2 = value
+        else:
+            return self.snd_wl2
+    def iss(self, value = None):
+        if value:
+            self.iss = value
+        else:
+            return self.iss
+    def rcv_nxt(self, value = None):
+        if value:
+            self.rcv_nxt = value
+        else:
+            return self.rcv_nxt
+    def rcv_wnd(self, value = None):
+        if value:
+            self.rcv_wnd = value
+        else:
+            return self.rcv_wnd
+    def rcv_up(self, value = None):
+        if value:
+            self.rcv_up = value
+        else:
+            return self.rcv_up
+    def irs(self, value = None):
+        if value:
+            self.irs = value
+        else:
+            return self.irs
+
