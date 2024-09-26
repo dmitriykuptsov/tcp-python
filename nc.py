fe2e517b95fb631928044073f4c97db84881df92
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2024-09-26 10:40:51 +0100

    Added arg parser

--- nc.py
+++ nc.py
@@ -22,18 +22,37 @@ __maintainer__ = "Dmitriy Kuptsov"
 __email__ = "dmitriy.kuptsov@strangebit.io"
 __status__ = "development"
 
+# TCP and IP packets
 from packets import *
+# TCP implmentation
 from tcp import *
+# TCP utilities
 from utils import TCPUtils
+# Timing
 from time import sleep
+# Arguments parser
+import argparse
+
+
+parser = argparse.ArgumentParser(
+                        prog='nc',
+                        description='Netcat for sending raw bytes over the network to the remote machine')
+
+parser.add_argument("--src", dest="src", required=True, help="Source address")
+parser.add_argument("--dst", dest="dst", required=True, help="Destination address")
+parser.add_argument("--source-port", dest="sport", required=True, help="Source port", type=int)
+parser.add_argument("--destination-port", dest="dport", required=True, help="Destination port", type=int)
+parser.add_argument("-l", required=True, help="Listen mode", action="store_true")
+args = parser.parse_args()
+
 tcp = TCP()
-tcp.open("127.0.0.1", "127.0.0.1", 45000, 1001, listen=False)
+tcp.open(args.src, args.dst, args.sport, args.dport, listen=args.l)
 #print(TCPUtils.generate_isn(0, "localhost", "localhost", 22, 45000))0
 print("------------------------------------------")
 tcp.send(bytearray([ord("H"), ord("E"), ord("L"), ord("L"), ord("O"), ord(" "), ord("W"), ord("O"), ord("R"), ord("L"), ord("D"), ord("\n")]))
 sleep(5)
 tcp.send(bytearray([ord("H"), ord("E"), ord("L"), ord("L"), ord("O"), ord(" "), ord("W"), ord("O"), ord("R"), ord("L"), ord("D"), ord("\n")]))
 while True:
-    print("SEND LOOP")
+    #print("SEND LOOP")
     #tcp.send(bytearray([chr("H"), chr("E"), chr("L"), chr("L"), chr("O")]))
     sleep(1)
